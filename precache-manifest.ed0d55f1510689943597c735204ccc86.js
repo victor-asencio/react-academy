9a05deae2804f01ef32c3243fb28ef888d49754c
Victor Asencio <victor.asencio@globant.com>
2020-05-28 10:12:11 -0300

    Updates

---
+++ precache-manifest.ed0d55f1510689943597c735204ccc86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a31429549addfc45900d3eff9a03f6",
+    "url": "/react-academy/index.html"
+  },
+  {
+    "revision": "94f4cea9f290397ce983",
+    "url": "/react-academy/static/css/main.5131e550.chunk.css"
+  },
+  {
+    "revision": "f65db2ad929f425d1b23",
+    "url": "/react-academy/static/js/2.bbc86a38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-academy/static/js/2.bbc86a38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94f4cea9f290397ce983",
+    "url": "/react-academy/static/js/main.df9f53b1.chunk.js"
+  },
+  {
+    "revision": "58b8ec8648ded6d36059",
+    "url": "/react-academy/static/js/runtime-main.dbed68a5.js"
+  }
+]);
